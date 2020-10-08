0fca3baf20290ddaa29ce93a1414ed24ab8057cc
ellag7121 <ellag7121@wrdsb.ca>
2020-10-08 13:51:46 +0000

    finished a6q4!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * asks the user for 2 vectors, tells them the dot value of both vectors
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //initialize the variables
+    int dotProduct; //the variable to hold the dot product
+
+    //create the arrays to hold the dot values
+    int[] vectors1 = new int[3];
+    int[] vectors2 = new int[3];
+
+    //ask the user for the first coordinates
+    System.out.println("Please enter the 3 values for the first vector");
+    for(int i = 0; i < vectors1.length; i++){
+      //store the user input in the arrays
+      vectors1[i] = input.nextInt();
+    }
+    //ask the user for the second coordinates
+    System.out.println("Please enter the 3 values for the second vector");
+    for(int i = 0; i < vectors2.length; i++){
+      //store the user input in the arrays
+      vectors2[i] = input.nextInt();
+    }
+
+    //get the dot product
+    dotProduct = (vectors1[0] * vectors2[0]) + (vectors1[1] * vectors2[1]) + (vectors1[2] * vectors2[2]);
+
+    //tell the user the dot product
+    System.out.println("The dot product is " + dotProduct);
+
+    
     
   }
 }
